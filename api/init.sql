c385e50b986966907db10758c96e945f16ec31d4
seetj <jasonkseet@gmail.com>
2023-11-16 16:22:58 -0500

    added offer and status tables

--- api/init.sql
+++ api/init.sql
@@ -1,30 +1,48 @@
-DROP TABLE IF EXISTS plants
-DROP TABLE IF EXISTS users
+DROP TABLE IF EXISTS plants;
+DROP TABLE IF EXISTS users;
 
-CREATE TABLE plants (
-    id INTEGER NOT NULL UNIQUE,
-    name VARCHAR(50) NOT NULL,
-    source VARCHAR(100) NOT NULL,
-    common_name VARCHAR NOT NULL,
-    type VARCHAR NOT NULL,
-    cycle  VARCHAR NOT NULL,
-    watering VARCHAR,
-    sunlight VARCHAR,
-    indoor BOOLEAN NOT NULL,
-    care_level VARCHAR NOT NULL,
-    maintenance VARCHAR NOT NULL,
-    description TEXT NOT NULL,
-    hardiness VARCHAR NOT NULL,
-    original_url VARCHAR NOT NULL,
-    dimensions VARCHAR NOT NULL
-    );
 
 CREATE TABLE users (
     id INTEGER NOT NULL UNIQUE,
     name INTEGER NOT NULL,
-    email VARCHAR NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
     username VARCHAR(20) NOT NULL,
-    password VARCHAR(20) NOT NULL,
-    zipcode INTEGER NOT NULL
+    password VARCHAR(255) NOT NULL,
+    zipcode VARCHAR(10) NOT NULL,
+    );
+
+
+CREATE TABLE status ( 
+    id INTEGER NOT NULL UNIQUE,
+    status VARCHAR(50) NOT NULL,
+    );
+
+    
+CREATE TABLE plants (
+    id INTEGER NOT NULL UNIQUE,
+    name VARCHAR(50) NOT NULL,
+    source VARCHAR(100) NOT NULL,
+    common_name VARCHAR(50) NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    cycle  VARCHAR(50) NOT NULL,
+    watering VARCHAR(50),
+    sunlight VARCHAR(50),
+    indoor BOOLEAN NOT NULL,
+    care_level VARCHAR(50) NOT NULL,
+    maintenance VARCHAR(50) NOT NULL,
+    description TEXT NOT NULL,
+    hardiness VARCHAR(50) NOT NULL,
+    original_url VARCHAR(50) NOT NULL,
+    dimensions VARCHAR(50) NOT NULL,
+    owner_id INTEGER NOT NULL REFERENCES users(id),
+    status VARCHAR(50) NOT NULL REFERENCES  status(id)
+    );
+
+
+CREATE TABLE offer (
+    id INTEGER NOT NULL UNIQUE,
+    price INTEGER NOT NULL,
+    buyer_id INTEGER NOT NULL REFERENCES users(id)
+    plant_id INTEGER NOT NULL REFERENCES plants(id)
     );
 
--- init.sql
+++ init.sql
@@ -1,6 +1,23 @@
 DROP TABLE IF EXISTS plants;
 DROP TABLE IF EXISTS users;
 
+
+CREATE TABLE users (
+    id INTEGER NOT NULL UNIQUE,
+    name INTEGER NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    zipcode VARCHAR(10) NOT NULL,
+    );
+
+
+CREATE TABLE status ( 
+    id INTEGER NOT NULL UNIQUE,
+    status VARCHAR(50) NOT NULL,
+    );
+
+    
 CREATE TABLE plants (
     id INTEGER NOT NULL UNIQUE,
     name VARCHAR(50) NOT NULL,
@@ -14,29 +31,18 @@ CREATE TABLE plants (
     care_level VARCHAR(50) NOT NULL,
     maintenance VARCHAR(50) NOT NULL,
     description TEXT NOT NULL,
-<<<<<<< HEAD
     hardiness VARCHAR(50) NOT NULL,
     original_url VARCHAR(50) NOT NULL,
     dimensions VARCHAR(50) NOT NULL,
-    owner_id INTEGER NOT NULL REFERENCES users(id)
-=======
-    hardiness VARCHAR NOT NULL,
-    original_url VARCHAR NOT NULL,
-    dimensions VARCHAR NOT NULL
->>>>>>> e909eae6866cf8937bb8cc6db9b47204bd2b75e9
+    owner_id INTEGER NOT NULL REFERENCES users(id),
+    status VARCHAR(50) NOT NULL REFERENCES  status(id)
     );
 
-CREATE TABLE users (
+
+CREATE TABLE offer (
     id INTEGER NOT NULL UNIQUE,
-    name INTEGER NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    username VARCHAR(20) NOT NULL,
-<<<<<<< HEAD
-    password VARCHAR(255) NOT NULL,
-    zipcode VARCHAR(10) NOT NULL,
-=======
-    password VARCHAR(20) NOT NULL,
-    zipcode INTEGER NOT NULL
->>>>>>> e909eae6866cf8937bb8cc6db9b47204bd2b75e9
+    price INTEGER NOT NULL,
+    buyer_id INTEGER NOT NULL REFERENCES users(id)
+    plant_id INTEGER NOT NULL REFERENCES plants(id)
     );
 
