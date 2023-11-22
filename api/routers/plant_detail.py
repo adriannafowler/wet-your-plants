084b4c0387e8088d8c40e959c2be7f367ffe3d53
Adrianna <adrianna.j.fowler@gmail.com>
2023-11-22 15:28:51 -0500

    updating plant-detail routes and queries

--- api/routers/plant_detail.py
+++ api/routers/plant_detail.py
@@ -5,11 +5,3 @@ from queries.plant_detail import PlantIn, PlantRepository
 router = APIRouter()
 
 
-
-@router.post("/greenhouse/{user_id}/")
-def create_plant(plant:PlantIn,
-                user_id: int,
-                repo: PlantRepository = Depends()
-                ):
-    print('plant', plant)
-    return repo.create(plant, user_id)
