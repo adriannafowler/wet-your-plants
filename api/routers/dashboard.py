362811ce04bcf403617db744b34804191434a558
Adrianna <adrianna.j.fowler@gmail.com>
2023-12-05 19:16:25 -0500

    requirements.txt hopefully fixed, started working on implementation of auth in routers, unit test added

--- api/routers/dashboard.py
+++ api/routers/dashboard.py
@@ -2,7 +2,25 @@ from fastapi import APIRouter, Depends
 from queries.dashboard import TodoRepository
 from models import TodoIn, CompleteIn, TodoOut
 from typing import List
+from jwtdown_fastapi.authentication import Token
+from .auth import authenticator
 
+
+class AccountToken(Token):
+    account: AccountOut
+
+
+@router.get("/token", response_model=AccountToken | None)
+async def get_token(
+    request: Request,
+    account: Account = Depends(authenticator.try_get_current_account_data)
+) -> AccountToken | None:
+    if account and authenticator.cookie_name in request.cookies:
+        return {
+            "access_token": request.cookies[authenticator.cookie_name],
+            "type": "Bearer",
+            "account": account,
+        }
 
 router = APIRouter()
 
@@ -10,6 +28,7 @@ router = APIRouter()
 @router.get("/dashboard/", response_model=List[TodoOut])
 def get_all_todos(
     user_id: int,
+    account_data: dict(Depends(authenticator.try_get_current_account_data))
     repo: TodoRepository = Depends(),
 ) -> List[TodoOut]:
     return repo.get_all(user_id)
