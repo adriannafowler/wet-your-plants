3830dcdac6059b8fce73f1a193c3448f461b716e
deonnguyen <deonhnguyen@gmail.com>
2023-12-07 16:24:17 -0600

    deleted nav.jsx

--- ghi/src/main/home.jsx
+++ ghi/src/main/home.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './home.css';
-import Nav from '/.nav';
 
 const Homepage = () => {
   // Plant data for content cards to match the design mockup
   const contentData = [
-    { id: 1, imageUrl: 'https://via.placeholder.com/300x200.png?text=Plant+1', title: 'Succulent Plant' },
-    { id: 2, imageUrl: 'https://via.placeholder.com/300x200.png?text=Plant+2', title: 'Fern Plant' },
-    { id: 3, imageUrl: 'https://via.placeholder.com/300x200.png?text=Plant+3', title: 'Cactus Plant' },
-    // Add more data as needed
+    // ... your content data
   ];
 
   return (
-    <nav>
-    <div className="homepage">
-      {contentData.map((item) => (
-        <div key={item.id} className="content-card">
-          <img src={item.imageUrl} alt={item.title} />
-          <div className="card-content">
-            <h3>{item.title}</h3>
-            {/* Add more content elements here if needed */}
+    <>
+      <div className="header">
+        <h1 className="title">Home Page</h1>
+        <Link to="/signin" className="sign-in-button">Sign In</Link>
+      </div>
+      <div className="homepage">
+        {contentData.map((item) => (
+          <div key={item.id} className="content-card">
+            <img src={item.imageUrl} alt={item.title} />
+            <div className="card-content">
+              <h3>{item.title}</h3>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
-    </nav>
+        ))}
+      </div>
+    </>
   );
 };
 
