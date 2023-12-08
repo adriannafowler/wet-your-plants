b11a4c881e8df145bd6275a35b52929b7b7feaea
seetj <jasonkseet@gmail.com>
2023-12-07 20:17:26 -0500

    minor tweak

--- ghi/src/main/home.jsx
+++ ghi/src/main/home.jsx
@@ -1,31 +1,43 @@
-import React from 'react';
-import './home.css';
-import Nav from '/.nav';
+import React from 'react'
+import './home.css'
+import Nav from '/.nav'
 
 const Homepage = () => {
-  // Plant data for content cards to match the design mockup
-  const contentData = [
-    { id: 1, imageUrl: 'https://via.placeholder.com/300x200.png?text=Plant+1', title: 'Succulent Plant' },
-    { id: 2, imageUrl: 'https://via.placeholder.com/300x200.png?text=Plant+2', title: 'Fern Plant' },
-    { id: 3, imageUrl: 'https://via.placeholder.com/300x200.png?text=Plant+3', title: 'Cactus Plant' },
-    // Add more data as needed
-  ];
+    // Plant data for content cards to match the design mockup
+    const contentData = [
+        {
+            id: 1,
+            imageUrl: 'https://via.placeholder.com/300x200.png?text=Plant+1',
+            title: 'Succulent Plant',
+        },
+        {
+            id: 2,
+            imageUrl: 'https://via.placeholder.com/300x200.png?text=Plant+2',
+            title: 'Fern Plant',
+        },
+        {
+            id: 3,
+            imageUrl: 'https://via.placeholder.com/300x200.png?text=Plant+3',
+            title: 'Cactus Plant',
+        },
+        // Add more data as needed
+    ]
 
-  return (
-    <nav>
-    <div className="homepage">
-      {contentData.map((item) => (
-        <div key={item.id} className="content-card">
-          <img src={item.imageUrl} alt={item.title} />
-          <div className="card-content">
-            <h3>{item.title}</h3>
-            {/* Add more content elements here if needed */}
-          </div>
-        </div>
-      ))}
-    </div>
-    </nav>
-  );
-};
+    return (
+        <nav>
+            <div className="homepage">
+                {contentData.map((item) => (
+                    <div key={item.id} className="content-card">
+                        <img src={item.imageUrl} alt={item.title} />
+                        <div className="card-content">
+                            <h3>{item.title}</h3>
+                            {/* Add more content elements here if needed */}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </nav>
+    )
+}
 
-export default Homepage;
+export default Homepage
