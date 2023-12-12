f8789c8999b5199dc22b291a0d0f1b354de32007
Adrianna <adrianna.j.fowler@gmail.com>
2023-12-12 15:59:59 -0500

    got dashboard delete and update functionality working

--- ghi/src/caredashboard/dailytodolist.jsx
+++ ghi/src/caredashboard/dailytodolist.jsx
@@ -14,7 +14,7 @@ import './dashboard.css'
 
 const DailyTodoList = ({ todos, setTodos, onClose, onAddTodo }) => {
     const [task, setTask] = useState('')
-
+    const [todoID, setTodoId] = useState('')
     const addTodo = () => {
         if (task.trim() !== '') {
             const newTodo = { text: task, complete: false }
@@ -23,69 +23,96 @@ const DailyTodoList = ({ todos, setTodos, onClose, onAddTodo }) => {
         }
     }
 
-    const toggleTodo = (index) => {
+    const toggleTodo = (todoId) => {
+        // Find the todo item using todoId
+        const todoToUpdate = todos.find(todo => todo.id === todoId);
+        if (!todoToUpdate) return;
+
         const updatedTodo = {
-            ...todos[index],
-            complete: !todos[index].complete,
-        }
-        fetch(
-            `http://localhost:8000/dashboard/complete/?todo_id=${todos[index].id}`,
-            {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                credentials: 'include',
-                body: JSON.stringify({
-                    complete: updatedTodo.complete,
-                }),
+            ...todoToUpdate,
+            complete: !todoToUpdate.complete,
+        };
+
+        const response =fetch(`http://localhost:8000/dashboard/complete/${todoId}/`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+            body: JSON.stringify({
+                complete: updatedTodo.complete,
+            }),
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Failed to update todo completion status');
             }
-        )
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Failed to update todo completion status')
-                }
-                return response.json()
-            })
-            .then((updatedTodo) => {
-                // Update the state only after the server confirms the change
-                const newTodos = [...todos]
-                newTodos[index] = updatedTodo
-                // Assuming there's a state setter function provided, e.g., setTodos
-                setTodos(newTodos)
-            })
-            .catch((error) => {
-                console.error('Error updating todo:', error)
-            })
-    }
+            return response.json();
+        })
+        .then((updatedTodoFromServer) => {
+            // Update the state only after the server confirms the change
+            const newTodos = todos.map(todo =>
+                todo.id === todoId ? updatedTodoFromServer : todo
+            );
+            setTodos(newTodos);
+        })
+        .catch((error) => {
+            console.error('Error updating todo:', error);
+        });
+    };
 
-    const removeTodo = (index) => {
-        // Remove the todo from the server
-        // ...
+    // const removeTodo = (todo.id) => {
+    //     // Remove the todo from the server
+    //     // ...
 
-        const newTodos = [...todos]
-        newTodos.splice(index, 1)
+        // const newTodos = [...todos]
+        // newTodos.splice(index, 1)
         // setTodos(newTodos) // this needs to be passed in from parent component as a prompt?
-    }
+
+        const removeTodo = async (todoId) => {
+            if (!todoId) return;
+            try {
+                const response = await fetch(
+                    `http://localhost:8000/dashboard/${todoId}/`,
+                    {
+                        method: 'DELETE',
+                        headers: { 'Content-Type': 'application/json' },
+                        credentials: 'include',
+                    }
+                );
+
+                if (!response.ok) {
+                    throw new Error('Failed to delete the todo.');
+                }
+                console.log('Todo deleted successfully');
+
+                // Update the state to reflect the deleted todo
+                const newTodos = todos.filter(todo => todo.id !== todoId);
+                setTodos(newTodos);
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+
     return (
         <div className="todo-list">
-            <TextField
+            {/* <TextField
                 label="Add a task"
                 value={task}
                 onChange={(e) => setTask(e.target.value)}
             />
             <Button className="button" onClick={addTodo}>
                 Add
-            </Button>
+            </Button> */}
 
             {todos.length > 0 && (
                 <List>
-                    {todos.map((todo, index) => (
-                        <ListItem key={index} className="todo-item">
+                    {todos.map((todo) => (
+                        <ListItem key={todo.id} className="todo-item">
                             <Checkbox
                                 className="checkbox"
                                 checked={todo.complete}
-                                onClick={() => toggleTodo(index)}
+                                onClick={() => toggleTodo(todo.id)}
                             />
                             <ListItemText
                                 primary={todo.todo}
@@ -95,7 +122,7 @@ const DailyTodoList = ({ todos, setTodos, onClose, onAddTodo }) => {
                                 <IconButton
                                     edge="end"
                                     className="delete-button"
-                                    onClick={() => removeTodo(index)}
+                                    onClick={() => removeTodo(todo.id)}
                                 >
                                     <DeleteIcon />
                                 </IconButton>
@@ -113,4 +140,3 @@ const DailyTodoList = ({ todos, setTodos, onClose, onAddTodo }) => {
 }
 
 export default DailyTodoList
-
