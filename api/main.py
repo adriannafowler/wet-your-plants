1f0f9bcc8eee3a6bd7390a7f827fe4b19650bbc9
Adrianna <adrianna.j.fowler@gmail.com>
2023-12-13 10:06:25 -0500

    updated main.py and gitlab yaml to try and fix cors error

--- api/main.py
+++ api/main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.environ.get("CORS_HOST", "http://localhost:3000")],
+    allow_origins=[os.environ.get("CORS_HOST", "https://the-horticoders.gitlab.io/wet-your-plants/", "http://localhost:3000")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
